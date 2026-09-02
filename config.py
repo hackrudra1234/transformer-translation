c251e10efbc8110d73c4dc79cf2f41aa95c337ba
hackrudra1234 <rudrasen512@gmail.com>
2026-09-02 13:00:53 +0530

    LR

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ DROPOUT = 0.1
 # -----------------------------
 # Training
 # -----------------------------
-LEARNING_RATE = 0.00025
+LEARNING_RATE = 0.00075
 EPOCHS = 10
 LR_SCHEDULE = "constant"
 
