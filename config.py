c2349e16aade40bf68129dd1dcb4c9dc182b8b8b
hackrudra1234 <rudrasen512@gmail.com>
2026-09-02 15:45:35 +0530

    BPE

--- config.py
+++ config.py
@@ -42,7 +42,7 @@ WEIGHT_TYING = False
 # -----------------------------
 # Inference
 # -----------------------------
-MAX_LEN = 40
+MAX_LEN = 60
 BLEU_MAX_EXAMPLES = 100
 BEAM_SIZE = 4
 LENGTH_PENALTY_ALPHA = 0.6
